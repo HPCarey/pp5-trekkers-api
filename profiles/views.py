dd1632aa7467a98518219a48378adaf5ea4e2330
HPCarey <hannahpcarey@gmail.com>
2023-05-16 13:03:14 +0000

    install drf, create ProfileList view, create and add urls file and file path

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Profile
 
-# Create your views here.
+
+class ProfileList(APIView):
+    """
+    Defines get method and
+    returns a list of the Profiles
+    """
+    def get(self, request):
+        profiles = Profile.objects.all()
+        return Response(profiles)
