c568cab4f73eb7e0894fdb3a29f95923dc2bdba2
HPCarey <hannahpcarey@gmail.com>
2023-05-17 16:18:16 +0000

    add post mehtod to PostList view and permissions, adjust rating fieldin models.py

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Post(models.Model):
@@ -37,7 +38,9 @@ class Post(models.Model):
         max_length=300,
         choices=DIFFICULTY_CHOICES,
     )
-    rating = models.IntegerField()
+    rating = models.PositiveIntegerField(
+        default=0, validators=[MinValueValidator(1), MaxValueValidator(5)]
+        )
     image = models.ImageField(
         upload_to='images/', default='../default_post_cgvnmr', blank=True
     )
