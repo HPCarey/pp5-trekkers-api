cab3cbe092d1f7cff5e9b5bc04da3bbb93e61f7c
HPCarey <hannahpcarey@gmail.com>
2023-05-17 11:05:31 +0000

    add PostDetail view, urls path and test in browser for valid and invlaid id

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -7,9 +8,28 @@ from .serializers import ProfileSerializer
 class ProfileList(APIView):
     """
     Defines get method and
-    returns a list of the Profiles
+    returns a list of the Profiles.
+    No post method as profile creation is handled by django signals
     """
     def get(self, request):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
+
+
+class ProfileDetail(APIView):
+    """
+    Returns an individual profile using it's primary key.
+    If Profile does not exist return 404
+    """
+    def get_object(self, pk):
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
         return Response(serializer.data)
