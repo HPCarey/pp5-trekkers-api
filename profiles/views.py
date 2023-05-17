722d8e9a490d6185aaaf16def8ba4155cd1824e0
HPCarey <hannahpcarey@gmail.com>
2023-05-17 11:16:47 +0000

    add put method for detail view

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -20,8 +21,13 @@ class ProfileList(APIView):
 class ProfileDetail(APIView):
     """
     Returns an individual profile using it's primary key.
-    If Profile does not exist return 404
+    If Profile does not exist returns.
+    Put method to fetch profile by id and manage data request.
+    If data is invalid return 404.
+    If data is valid, save and return instance.
     """
+    serializer_class = ProfileSerializer
+
     def get_object(self, pk):
         try:
             profile = Profile.objects.get(pk=pk)
@@ -33,3 +39,11 @@ class ProfileDetail(APIView):
         profile = self.get_object(pk)
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
