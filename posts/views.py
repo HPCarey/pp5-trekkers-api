8a481686423f1b8159f4dc8eea8fa36167a6980f
HPCarey <hannahpcarey@gmail.com>
2023-05-17 16:43:09 +0000

    add PostDetail view with get, put and get_object methods, wire up urls

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,10 @@
+from django.http import Http404
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Post
 from .serializers import PostSerializer
+from trekkers.permissions import IsOwnerOrReadOnly
 
 
 class PostList(APIView):
@@ -16,7 +18,6 @@ class PostList(APIView):
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly
     ]
-
 
     def get(self, request):
         posts = Post.objects.all()
@@ -35,6 +36,44 @@ class PostList(APIView):
                 serializer.data, status=status.HTTP_201_CREATED
             )
 
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class PostDetail(APIView):
+    """
+    Returns an individual post using it's primary key.
+    Checks if user is owner, if not allow readonly.
+    If Post doesn't exist return 404.
+    Put method allows post owner to update post.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = PostSerializer
+
+    def get_object(self, pk):
+        try:
+            post = Post.objects.get(pk=pk)
+            self.check_object_permissions(self.request, post)
+            return post
+        except Post.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
         return Response(
             serializer.errors, status=status.HTTP_400_BAD_REQUEST
         )
