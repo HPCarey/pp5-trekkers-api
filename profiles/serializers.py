62de3bc26de934d8ed0e413e0917b26de1125513
HPCarey <hannahpcarey@gmail.com>
2023-05-18 15:17:27 +0000

    add get_following_id method and add following_id to fields list in ProfileSerializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,19 +1,37 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     """Converts Profile model instance to JSON"""
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_following_id(self, obj):
+        """
+        Gets current user and check if authenticated.
+        Check if logged in user is follwoing any profiles by
+        filtering Follower object.
+        If yes the profile instance is returned
+        """
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            print(following)
+            return following.id if following else None
+        return None
+
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name', 'content', 
-            'image', 'is_owner'
+            'image', 'is_owner', 'following_id'
         ]
